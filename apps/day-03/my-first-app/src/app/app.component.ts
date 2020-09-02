4e2d1822669e698735a1135cdf0a31287a3efbf2
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-09-02 09:51:59 +0530

    add app for day 3

---
+++ apps/day-03/my-first-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',  // MANDATORY
+  templateUrl: './app.component.html', // MANDATORY
+  styleUrls: ['./app.component.css']   // OPTIONAL
+})
+export class AppComponent {
+  title = 'my-first-app';
+}
